c9d7811f0546a0644940f7dea88c39694e794e5a
kali <kali@kali>
2020-09-09 12:16:31 -0500

    test again

--- module3/scapy_portscanner.py
+++ module3/scapy_portscanner.py
@@ -6,4 +6,12 @@ import sys
 from scapy.all import *
 
 if len(sys.argv) != 4:
-    
+    print("Usage: %s target startport endport" %(sys.argv[0]))
+    sys.exit(0)
+
+target = str(sys.argv[1])
+startport = int(sys.argv[2])
+endport = int(sys.argv[3])
+
+print('Scanning '+target+' for open TCP ports\n')
+
