ad34d537e22f8924c4ddfd74d9373c1947f23e4a
kali <kali@kali>
2020-09-11 14:31:58 -0500

    socket server threading

--- module3/socketserver_threading.py
+++ module3/socketserver_threading.py
@@ -7,45 +7,42 @@ import socketserver
 class ThreadedTCPRequestHandler(socketserver.BaseRequestHandler):
 
     def handle(self):
-        data = self.request.recv(1024)
+        data = str(self.request.recv(1024), 'ascii')
         cur_thread = threading.current_thread()
-        response = "{}: {}".format(cur_thread.name, data)
+        response = bytes("{}: {}".format(cur_thread.name, data), 'ascii')
         self.request.sendall(response)
 
-class ThreadedTCPServer(socketserver.ThreadingMixIn,socketserver.TCPServer):
+class ThreadedTCPServer(socketserver.ThreadingMixIn, socketserver.TCPServer):
     pass
 
 def client(ip, port, message):
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.connect((ip, port))
-    try:
-        sock.sendall(message)
-        response = sock.recv(1024)
-        print("Recieved: {}").format(response)
-    finally:
-        sock.close()
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+        sock.connect((ip, port))
+        sock.sendall(bytes(message, 'ascii'))
+        response = str(sock.recv(1024), 'ascii')
+        print("Received: {}".format(response))
 
 if __name__ == "__main__":
-    # port 0 means to select an arbitrary unused port
+    # Port 0 means to select an arbitrary unused port
     HOST, PORT = "localhost", 0
 
-    server = ThreadedTCPServer((HOST, PORT),ThreadedTCPRequestHandler)
-    ip, port = server.server_address
+    server = ThreadedTCPServer((HOST, PORT), ThreadedTCPRequestHandler)
+    with server:
+        ip, port = server.server_address
 
-    # start a thread with the server -- thread will start one
-    # more thread for each request
-    server_thread = threading.Thread(target=server.serve_forever)
-    # exit the server thread when main thread terms
-    server_thread.daemon = True
-    server_thread.start()
-    print("Server loop running in thread:", server_thread.name)
+        # Start a thread with the server -- that thread will then start one
+        # more thread for each request
+        server_thread = threading.Thread(target=server.serve_forever)
+        # Exit the server thread when the main thread terminates
+        server_thread.daemon = True
+        server_thread.start()
+        print("Server loop running in thread:", server_thread.name)
 
-    client(ip, port, "Hello World 1")
-    client(ip, port, "Hello World 2")
-    client(ip, port, "Hello World 3")
+        client(ip, port, "Hello World 1")
+        client(ip, port, "Hello World 2")
+        client(ip, port, "Hello World 3")
 
-    server.shutdown()
-    server.server_close()
+        server.shutdown()
 
 
 
