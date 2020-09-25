4a9271427eccb8e25b39cfc7f8da17815635e129
kali <kali@kali>
2020-09-25 13:23:23 -0500

    netifaces subnet finder

---
+++ module3/netifacesSubnetFinder.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+# apt-get install python-netifaces
+
+import netifaces
+import socket
+
+print('Interfaces available: ')
+print(netifaces.interfaces())
+eth0_addresses = netifaces.ifaddresses("eth0")
+interface_info = eth0_addresses[socket.AF_INET][0]
+print(interface_info['addr'])
+print(interface_info['netmask'])
