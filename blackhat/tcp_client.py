514498bd0613c46df1bbabb33a7090269e4166a9
kali <kali@kali>
2020-09-30 12:24:11 -0500

    tcp client

---
+++ blackhat/tcp_client.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+import socket
+
+target_host = "google.com"
+target_port = 80
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect((target_host,target_port))
+client.send(b"GET / HTTP/1.1\r\nHOST: google.com\r\n\r\n")
+
+response = client.recv(4098)
+
+print(response)
